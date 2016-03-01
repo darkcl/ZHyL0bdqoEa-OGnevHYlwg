965e834edf455b0cd99fb71531117fec7f6d9389
Yeung Yiu Hung <hkclex@gmail.com>
2016-03-01 22:05:18 +0800

    Inital Commit

---
+++ Example/Pods/Target Support Files/AfterShipKit/AfterShipKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AfterShipKit : NSObject
+@end
+@implementation PodsDummy_AfterShipKit
+@end
---
+++ Example/Pods/Target Support Files/AfterShipKit/AfterShipKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AfterShipKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char AfterShipKitVersionString[];
+
